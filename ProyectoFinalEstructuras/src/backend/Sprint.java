3731afef3265d84d9faa5862bc1b6b81dc0b79a8
Alek Castillo Bogantes <alekcastillovoca@hotmail.com>
2018-11-29 12:40:21 -0600

    Added base classes

---
+++ ProyectoFinalEstructuras/src/backend/Sprint.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+import java.util.Date;
+
+/**
+ *
+ * @author alekc
+ */
+public class Sprint {
+    int id;
+    String title, description;
+    Date startDate, endDate;
+    //tasks
+}
